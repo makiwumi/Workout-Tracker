fc88eebb09b76d157b171edbc56fa94a76439338
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-05-21 20:17:11 -0400

    added models and get/post in routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+//Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser");
 
+//Express app
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,14 +13,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//mongo db
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
+
+//starting server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
